04c2a6f20c9c85eeaa6d8bd975b3f29a1370ffbe
lucianocolin <lucianocolin99@gmail.com>
2022-09-03 16:02:22 -0300

    demo 1

--- juan-casanas-react/src/pages/Home.jsx
+++ juan-casanas-react/src/pages/Home.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import{ React, useEffect} from 'react';
 import '../css/Home.css';
 import juancasanas from '../imgs/Juan-Casañas.png';
+import juancasanas2 from '../imgs/Juan-Casañas2.png';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+
+  useEffect(()=>{
+    Aos.init({ duration: 1500 })
+  }, []);
+
   return (<>
   <div className='home'>
     <div className='home-cover-container'>
       <img src={juancasanas} alt="Juan Casañas" />
     </div>
-    <section className='home-p-container'>
-      <p>
+    <div className='home-main-img-container'>
+      <img src={juancasanas2} alt='Juan Casañas' />
+    </div>
+    <div className='home-p-container'>
+      <p data-aos='fade-up'>
         Soy ingeniero agrónomo, productor agropecuario y ex diputado
-        nacional por la provincia de Tucumán, miembro de la Unión Civica Radical.
+        nacional por la provincia de Tucumán, miembro de la Unión Civica Radical. <br /> <br />
+        Inicié mi actividad gremial en el ámbito agropecuario en la Federación Agraria Argentina
+        en 2008 y llegué a ser Secretario del Distrito 11 en Tucumán
       </p>
-    </section>
+      <p data-aos='fade-up'>
+        Asumí como diputado nacional en las elecciones legislativas del 2009
+        como uno de los 11 diputados de orígen agropecuario.
+      </p>
+    </div>
   </div>
   </>
   )
