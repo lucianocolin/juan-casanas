873d2b4bd105de537aba7e4257ad27565ecba52f
lucianocolin <lucianocolin99@gmail.com>
2022-11-12 14:58:41 -0300

    home demo done

--- juan-casanas-react/src/pages/Home.jsx
+++ juan-casanas-react/src/pages/Home.jsx
@@ -1,35 +1,34 @@
-import{ React, useEffect} from 'react';
+import{ React } from 'react';
 import '../css/Home.css';
 /* import juancasanas from '../imgs/Juan-Casañas.png'; */
-import juancasanas2 from '../imgs/Juan-Casañas2.png';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import asumiendo from '../imgs/asumiendo.jpg';
 
 const Home = () => {
 
-  useEffect(()=>{
-    Aos.init({ duration: 1500 })
-  }, []);
-
   return (<>
   <div className='home'>
+    <div className='introduction-container'>
+      <p>Ex diputado Nacional. Candidato a Intendente de Yerba Buena 2023</p>
+    </div>
     <div className='home-cover-container'>
-      {/* <img src={juancasanas} alt="Juan Casañas" /> */}
     </div>
     <div className='home-body-container'>
-      <div className='home-main-img-container'>
-        <img src={juancasanas2} alt='Juan Casañas' />
-      </div>
-      <div className='home-p-container'>
-        <p data-aos='fade-up'>
+      <div className='home-p-container' data-aos='fade-up'>
+        <p>
           Soy ingeniero agrónomo, productor agropecuario y ex diputado
           nacional por la provincia de Tucumán, miembro de la Unión Civica Radical. <br /> <br />
           Inicié mi actividad gremial en el ámbito agropecuario en la Federación Agraria Argentina
           en 2008 y llegué a ser Secretario del Distrito 11 en Tucumán
         </p>
-        <p data-aos='fade-up'>
+        <p>
           Asumí como diputado nacional en las elecciones legislativas del 2009
           como uno de los 11 diputados de orígen agropecuario.
+        </p>
+      </div>
+      <div className='home-main-img-container'>
+        <img src={asumiendo} alt='Juan Casañas' />
+        <p>Juan Casañas asumiendo como Coordinador 
+          de Cambio Rural
         </p>
       </div>
     </div>
