d3e75bc477d9b57a46cb66ad7f885cec8b0b968c
lucianocolin <lucianocolin99@gmail.com>
2022-11-16 12:27:31 -0300

    testing text color

--- juan-casanas-react/src/components/Header.jsx
+++ juan-casanas-react/src/components/Header.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import '../css/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'; 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (<>
       <div className='header'>
-        <h2>JUAN CASAÑAS</h2>
+        <h2>J C</h2>
         <nav>
           <input type="checkbox" id='check' />
           <label htmlFor="check" className='toggle-btn'>
             <FontAwesomeIcon icon={faBars} className='toggle-menu-icon'/>
           </label>
           <ul id='header-list' className='header-list'>
-            <li>Inicio</li>
-            <li>Sobre mí</li>
+            <li>
+              <NavLink to='/' className={({ isActive })=> isActive ? 'active header-link' : 'header-link'}>
+                Inicio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sobre-mi' className='header-link'>
+                Sobre Mí
+              </NavLink>
+            </li>
             <li>Mi trabajo</li>
             <li>Contacto</li>
           </ul>
