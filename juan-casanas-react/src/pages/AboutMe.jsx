63c9376e718dd4aa98505110ba6d0339c85c8c90
lucianocolin <lucianocolin99@gmail.com>
2022-11-16 17:22:11 -0300

    schooling and work section added

--- juan-casanas-react/src/pages/AboutMe.jsx
+++ juan-casanas-react/src/pages/AboutMe.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import '../css/AboutMe.css';
+import React from "react";
+import "../css/AboutMe.css";
 
 const AboutMe = () => {
-  return (<>
-    <div>
-      <div className='name-jfc'>
-        <h2>Juan Francisco Casañas</h2>
+  return (
+    <>
+      <div>
+        <div className="name-jfc">
+          <h2>Juan Francisco Casañas</h2>
+        </div>
+        <h4>
+          Padre de 3 hijos <br />
+          Ingeniero agrónomo <br />
+          Empresario <br />
+          Productor agropecuario
+        </h4>
+        <div className="about-me-cover"></div>
+        <div className="about-me-text">
+          <h2>Estudios</h2>
+          <p>
+            Realicé mis estudios primarios en el{" "}
+            <b>Colegio Salesiano Tulio García Fernández. </b>
+            Luego mi educación secundaria en el{" "}
+            <b>Colegio Nacional de Tucumán</b>. En 1992 me recibí de{" "}
+            <b>ingeniero agrónomo</b> de la
+            <b> Universidad Nacional de Tucumán</b>. Profesión que ejerzo.{" "}
+            <br />
+          </p>
+          <h2>Actividad en el campo</h2>
+          <p>
+            También soy <b>productor agropecuario </b>
+            con actividades en granos, limón y cerdos. Desde el año 2003 soy
+            miembro del Grupo El Palomar de la región del Noroeste de la{" "}
+            <b>
+              {" "}
+              Asociación Regional de Consorcios de Experimentación Agrícola{" "}
+            </b>
+            (AACREA). Como miembro de la asociación me desempeño como Vocal de
+            la Zona NOA.
+          </p>
+          <h2>Política</h2>
+          <p>
+            Inicié mi actividad política en el ámbito agropecuario en la
+            <b> Federación Agraria Argentina</b>, donde me afilié en
+            2008, llegando a ser Secretario del Distrito 11 en Tucumán.
+          </p>
+        </div>
       </div>
-      <h4>
-        Padre de 3 hijos <br />
-        Ingeniero agrónomo <br />
-        Empresario <br />
-        Productor agropecuario
-      </h4>
-      <div className='about-me-cover'></div>
-      <div className='about-me-text'>
-        <p>
-        Realicé mi educación secundaria en el <b>Colegio 
-        Nacional de Tucumán</b>. En 1992 me recibí de ingeniero agrónomo de la 
-        Universidad Nacional de Tucumán. Profesión que ejerzo. También soy productor agropecuario. 
-        En el año 2009 asumió su banca como diputado nacional por la provincia de Tucumán 
-        por el Acuerdo Cívico y Social, luego del paro agropecuario patronal en Argentina de 2008.
-        Desde el año 2003 es miembro del Grupo El Palomar de la región del Noroeste de la 
-        Asociación Regional de Consorcios de Experimentación Agrícola (AACREA). 
-        Como miembro CREA se desempeñó como Vocal de la Zona NOA.
-        </p>
-      </div>
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
